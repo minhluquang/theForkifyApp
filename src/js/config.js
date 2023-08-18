8d923de18000196135fb998e2f1ef9e85e2fd6f7
minhluquang <minhlq2911@gmail.com>
2023-08-18 12:37:27 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const KEY = '745eea9d-ea01-4871-bdcc-1b83444791b1';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
